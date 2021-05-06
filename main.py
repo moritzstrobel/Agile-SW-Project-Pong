16ef2e545897a527c3ad3316ae7482cf528cb4f8
Friedrich <fkgwelk@gmail.com>
2021-05-06 15:07:48 +0200

    ol

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame, math
+import random
 from win32api import GetSystemMetrics
 
 #Setup
@@ -24,30 +25,90 @@ screen=pygame.display.set_mode([screen_width, screen_height])
 
 #Rects
 player1 = pygame.Rect(10, screen_height/2 - 70, 10 , 140)
-player2 = pygame.Rect(screen_width - 20, screen_height/2 -70, 10 , 140)
+opponent = pygame.Rect(screen_width - 20, screen_height/2 -70, 10 , 140)
 ball = pygame.Rect(screen_width/2 -20, screen_height/2 -20, 20, 20)
+
+wand1 = pygame.Rect(0, 0, screen_width, 10)
+wand2 = pygame.Rect(0, screen_height - 10, screen_width, 10)
+
+powerup = pygame.Rect(0, 0, 20, 20)
+
+
+#PoerupRando
+
+def pukleinergroesser():
+    #logic fuer die Groessenaenderung bei Powerup
+
+def puschnellerlangsam():
+    #logic fuer die Schnelligkeit bei Powerup
+
+def powerupPicker():
+    which = random.choice((0,1))
+
+    if which == 0:
+        pukleinergroesser()
+
+    if which == 1:
+        puschnellerlangsam()
 
 #Movement
 
-ball_speedx = 4
-ball_speedy = 4
+ball_speedx = 4 * random.choice((1, -1))
+ball_speedy = 4 * random.choice((1, -1))
 player1_speed = 0
+opponent_speed = 0
 
-#Ball Animation
+#Score
+player_score = 0
+opponent_score = 0
+
+game_font = pygame.font.Font("freesansbold.ttf", 32)
+
+#Animation
+
 def ball_animation():
-    global ball_speedx, ball_speedy
+    global ball_speedx, ball_speedy, opponent_score, player_score
     ball.x += ball_speedx
     ball.y += ball_speedy
 
-    if ball.top <= 0 or ball.bottom >= screen_height:
+    if ball.colliderect(wand1) or ball.colliderect(wand2):
         ball_speedy *= -1
 
     if ball.left <= 0 or ball.right >= screen_width:
+        if ball.left <= 0:
+            player_score += 1
+        if ball.right >= screen_width:
+            opponent_score += 1
+        ball_restart()
+
+    if ball.colliderect(player1) or ball.colliderect(opponent):
         ball_speedx *= -1
 
-    if ball.colliderect(player1) or ball.colliderect(player2
-):
-        ball_speedx *= -1
+def player_animation():
+    player1.y += player1_speed
+    if player1.top <= wand1[3]:
+        player1.top = 0
+    if player1.bottom >= screen_height - wand2[3]:
+        player1.bottom = screen_height
+       
+
+def opponent_ai():
+    if ball.x > screen_width/2:
+        if opponent.top < ball.y:
+            opponent.top += opponent_speed
+        if opponent.bottom > ball.y:
+            opponent.bottom -= opponent_speed
+    if opponent.top <= 0:
+        opponent.top = 0
+    if opponent.bottom >= screen_height:
+        opponent.bottom = screen_height 
+
+def ball_restart():
+    global ball_speedx, ball_speedy
+    ball.center = (screen_width/2, screen_height/2)
+    ball_speedy *= random.choice((1, -1)) 
+    ball_speedx *= random.choice((1, -1))
+
 
 
 
@@ -74,24 +135,27 @@ while spielaktiv:
                  player1_speed += 6
 
         
-    ball_animation()
-    # if player1_speed!= 0 :
-    #     player1[1] += player1_speed
-    
-    player1.y += player1_speed
-    if player1.top <= 0:
-        player1.top = 0
-    if player1.bottom >= screen_height:
-        player1.bottom = screen_height  
+    ball_animation()  
+    player_animation()
+    opponent_ai()
+     
 
     #Visual
     screen.fill(SCHWARZ)
     pygame.draw.ellipse(screen, WEISS, ball)
     pygame.draw.rect(screen, WEISS, player1)
-    pygame.draw.rect(screen, WEISS, player2
-)
+    pygame.draw.rect(screen, WEISS, opponent)
+    pygame.draw.rect(screen, WEISS, wand1)
+    pygame.draw.rect(screen, WEISS, wand2)
+    pygame.draw.aaline(screen, WEISS, (screen_width / 2, 0), (screen_width / 2, screen_height))
 
-    
+    player_text = game_font.render(str(player_score), False, WEISS)
+    screen.blit(player_text, (screen_width/2 +10, screen_height/2))
+
+    opponent_text = game_font.render(str(opponent_score), False, WEISS)
+    screen.blit(opponent_text, (screen_width/2 -25, screen_height/2))
+
+
    
     #Update
     pygame.display.flip()
